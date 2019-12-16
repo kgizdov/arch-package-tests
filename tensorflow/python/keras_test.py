157fb883fd44426fb2b7b13216010e524b6ac5ae
Konstantin Gizdov <kgizdov@gmail.com>
2019-12-16 14:22:28 +0200

    update tensorflow python test

--- tensorflow/python/keras_test.py
+++ tensorflow/python/keras_test.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
-from keras.models import load_model
-model = load_model('keras_locallyconnected1d.h5')
-proba = model.predict_proba(np.random.normal(size = (10,6,1)))
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+print('Loading model test.h5')
+model = load_model('test.h5')
+print('Predicting on random input')
+proba = model.predict_proba(np.random.normal(size = (1,28,28)))
 print('Result is:\n{}'.format(proba))
 print('Success!')
