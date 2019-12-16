18eba83b071599bcb54069956f401b19ae33d4e1
Konstantin Gizdov <kgizdov@gmail.com>
2019-12-16 14:01:26 +0200

    update tensorflow basic test

--- tensorflow/basic/basic.cpp
+++ tensorflow/basic/basic.cpp
@@ -5,17 +5,17 @@
 #include "tensorflow/core/framework/tensor.h"
 
 int main() {
-  using namespace tensorflow;
-  using namespace tensorflow::ops;
-  Scope root = Scope::NewRootScope();
+  namespace tf = tensorflow;
+  namespace tfo = tensorflow::ops;
+  tf::Scope root = tf::Scope::NewRootScope();
   // Matrix A = [3 2; -1 0]
-  auto A = Const(root, { {3.f, 2.f}, {-1.f, 0.f} });
+  auto A = tfo::Const(root, { {3.f, 2.f}, {-1.f, 0.f} });
   // Vector b = [3 5]
-  auto b = Const(root, { {3.f, 5.f} });
+  auto b = tfo::Const(root, { {3.f, 5.f} });
   // v = Ab^T
-  auto v = MatMul(root.WithOpName("v"), A, b, MatMul::TransposeB(true));
-  std::vector<Tensor> outputs;
-  ClientSession session(root);
+  auto v = tfo::MatMul(root.WithOpName("v"), A, b, tfo::MatMul::TransposeB(true));
+  std::vector<tf::Tensor> outputs;
+  tf::ClientSession session(root);
   // Run and fetch v
   TF_CHECK_OK(session.Run({v}, &outputs));
   // Expect outputs[0] == [19; -3]
