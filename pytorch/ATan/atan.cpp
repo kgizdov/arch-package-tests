23e4304b9cf79f9b45a274076ec0908abaffbc1b
Konstantin Gizdov <kgizdov@gmail.com>
2020-03-25 19:25:59 +0200

    update for PyTorch 1.2+

--- pytorch/ATan/atan.cpp
+++ pytorch/ATan/atan.cpp
@@ -8,14 +8,14 @@ int main() {
     std::cout << "CUDA device is not available.\n";
   }
   try {
-    at::Tensor a = at::ones({2, 2}, at::kInt).cuda();
-    at::Tensor b = at::randn({2, 2}).cuda();
-    auto c = a + b.to(at::kInt).cuda();
+    at::Tensor a = torch::ones({2, 2}, torch::kInt).cuda();
+    at::Tensor b = torch::randn({2, 2}).cuda();
+    auto c = a + b.to(torch::kInt).cuda();
     std::cout << c << "\n";
   } catch (const c10::Error& e) {
-    at::Tensor a = at::ones({2, 2}, at::kInt);
-    at::Tensor b = at::randn({2, 2});
-    auto c = a + b.to(at::kInt);
+    at::Tensor a = torch::ones({2, 2}, torch::kInt);
+    at::Tensor b = torch::randn({2, 2});
+    auto c = a + b.to(torch::kInt);
     std::cout << c << "\n";
   }
   return 0;
--- pytorch/autograd/autograd.cpp
+++ pytorch/autograd/autograd.cpp
@@ -8,7 +8,7 @@ int main() {
     std::cout << "CUDA device is not available.\n";
   }
   try {
-    auto test = torch::ones({2, 2}, at::requires_grad()).cuda();
+    auto test = torch::ones({2, 2}, torch::TensorOptions().requires_grad(true).device(torch::kCUDA));
     if (test.device().type() == torch::kCUDA) {
       std::cout << "We are using CUDA\n";
     } else if (test.device().type() == torch::kCPU) {
@@ -16,13 +16,21 @@ int main() {
     } else {
       std::cout << "Uuuhm, we are supposed to run on CUDA GPU, but we are not even running on CPU.";
     }
-    auto a = torch::ones({2, 2}, at::requires_grad()).cuda();
-    auto b = torch::randn({2, 2}).cuda();
-    auto c = a + b;
-    c.backward(); // a.grad() will now hold the gradient of c w.r.t. a.
-    std::cout << c << "\n";
+    auto a = torch::ones({2, 2}, torch::TensorOptions().dtype(torch::kFloat).requires_grad(true).device(torch::kCUDA));
+    std::cout << "a: " << a << "\n";
+    auto b = torch::randn({2, 2}, torch::TensorOptions().dtype(torch::kFloat).device(torch::kCUDA));
+    std::cout << "b: " << b << "\n";
+    auto c = (a + b).sum();
+    std::cout << "(a + b).sum(): " << c << "\n";
+    c.backward();  // a.grad() will now hold the gradient of c w.r.t. a.
+    std::cout << "a.grad(): " << a.grad() << "\n";
+    torch::Tensor g_loss = torch::binary_cross_entropy(a, b);
+    g_loss.backward();
+    std::cout << "torch::binary_cross_entropy(a, b): " << g_loss << "\n";
+    std::cout << "a.grad(): " << a.grad() << "\n";
   } catch (const c10::Error& e) {
-    auto test = torch::ones({2, 2}, at::requires_grad());
+    std::cout << e.what() << "\n";
+    auto test = torch::ones({2, 2}, torch::TensorOptions().requires_grad(true).device(torch::kCPU));
     if (test.device().type() == torch::kCPU) {
       std::cout << "We are using CPU\n";
     } else if (test.device().type() == torch::kCUDA) {
@@ -30,11 +38,18 @@ int main() {
     } else {
       std::cout << "Uuuhm, we are supposed to run on CPU, but we are not even running on CUDA GPU.";
     }
-    auto a = torch::ones({2, 2}, at::requires_grad());
-    auto b = torch::randn({2, 2});
-    auto c = a + b;
-    c.backward(); // a.grad() will now hold the gradient of c w.r.t. a.
-    std::cout << c << "\n";
+    auto a = torch::ones({2, 2}, torch::TensorOptions().requires_grad(true).device(torch::kCPU));
+    std::cout << "a: " << a << "\n";
+    auto b = torch::randn({2, 2}, torch::TensorOptions().device(torch::kCPU));
+    std::cout << "b: " << b << "\n";
+    auto c = (a + b).sum();
+    std::cout << "(a + b).sum(): " << c << "\n";
+    c.backward();  // a.grad() will now hold the gradient of c w.r.t. a.
+    std::cout << "a.grad(): " << a.grad() << "\n";
+    torch::Tensor g_loss = torch::binary_cross_entropy(a, b);
+    g_loss.backward();
+    std::cout << "torch::binary_cross_entropy(a, b): " << g_loss << "\n";
+    std::cout << "a.grad(): " << a.grad() << "\n";
   }
   return 0;
 }
